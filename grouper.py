78c89ec9cb3bfffb63da9c8ae2a08a1f351e9c01
Wilmer Finol Inciarte <WilmerFinolInciarte@gmail.com>
2020-09-16 17:31:48 -0500

    Change program to process all parameters and sweeps at once

--- grouper.py
+++ grouper.py
@@ -6,6 +6,22 @@ import os
 
 WELLS = 384
 
+
+def choose_file():
+    csv_files = [name for name in os.listdir("./input/") if name.endswith(".csv")]
+    if len(csv_files) < 1:
+        sys.exit("No csv files found! Are you in the right directory?")
+
+    for i, name in enumerate(csv_files, start=1):
+        print( "({}) {}".format(i, name) )
+
+    index = int(input("Which file do you want to process? "))
+    while index < 1 or index > len(csv_files):
+        index = int(input("Try again. Which file do you want to process? "))
+
+    return csv_files[index-1]
+
+
 # Gets relevant data from the csv file
 # Needs: filename
 # Returns: Dataframe with relevant data
@@ -35,42 +51,59 @@ def get_relevant(filename):
         else:
             bool_mask.append(False)
 
-    return nav.loc['A01':, columns[bool_mask]], nav.iloc[0,2:2+num_param] # Parameter selection can probably be improved
+    return nav.loc['A01':, columns[bool_mask]], nav.iloc[0,2:2+num_param], num_sweeps # Parameter selection can probably be improved
 
 def main():
 
     # Choose file
-    csv_files = [name for name in os.listdir("./input/") if name.endswith(".csv")]
-    if len(csv_files) < 1:
-        sys.exit("No csv files found! Are you in the right directory?")
-
-    for i, name in enumerate(csv_files, start=1):
-        print( "({}) {}".format(i, name) )
-
-    index = int(input("Which file do you want to process? "))
-    while index < 1 or index > len(csv_files):
-        index = int(input("Try again. Which file do you want to process? "))
-
-    filename = csv_files[index-1]
-    rel_data, parameters = get_relevant("./input/" + filename)
-
-    # Choose parameter
-    for i, param in enumerate(parameters, start=1):
-        print("({}) {}".format(i, param))
-
-    index = int(input("\nWhich parameter do you want to process? "))
-    while index < 1 or index > len(parameters):
-        index = int(input("Try again. Which parameter do you want to process? "))
-
-    rel_arr = rel_data.iloc[:,index-1].to_numpy()
-
-    plate_cols = 24
-    plate_rows = 16
-    row_names = list(string.ascii_uppercase[:plate_rows])
-
-    clean = pd.DataFrame(data = rel_arr.reshape(plate_rows, plate_cols,order='F'), index=row_names, columns=range(1,plate_cols+1))
+    filename = choose_file()
+    rel_data, parameters, num_sweeps = get_relevant("./input/" + filename)
 
     os.makedirs(os.path.dirname("./output/"), exist_ok=True)
-    clean.to_csv("./output/clean_" + filename)
+
+    # Choose plate layout
+    plate_cols = 24 # int(input("How many columns? "))
+    plate_rows = 16 # int(input("How many rows? "))
+    row_names = list(string.ascii_uppercase[:plate_rows])
+
+    if not plate_cols * plate_rows == WELLS:
+        sys.exit("The number of columns and rows must multiply to 384.")
+
+    # Create directories for each parameter
+    path = "./output/" + filename[:-4] + "/"
+    for index, param in enumerate(parameters, start=0):
+        param_path = path + param + "/"
+        if not os.path.isdir(param_path):
+            os.makedirs(param_path, exist_ok=True);
+
+        for sweep in range(1, num_sweeps+1):
+            filepath = param_path + "/Sweep{:03}.csv".format(sweep)
+            rel_arr = rel_data.iloc[:,len(parameters)*(sweep-1) + (index-1)].to_numpy()
+            clean = pd.DataFrame(data = rel_arr.reshape(plate_rows, plate_cols,order='F'), index=row_names, columns=range(1,plate_cols+1))
+
+            clean.to_csv(filepath)
+
+
+
+    # Choose sweep
+#    sweep = int(input("\nThere are {} sweeps. Which sweep do you want to process? ".format(num_sweeps)))
+#    while sweep < 1 or sweep > num_sweeps:
+#         sweep = int(input("Try again. Which sweep do you want to process? "))
+#    filename = filename[:-4] + "_Sweep{:03}".format(sweep)
+#
+#    # Choose parameter
+#    for i, param in enumerate(parameters, start=1):
+#        print("({}) {}".format(i, param))
+#
+#    index = int(input("\nWhich parameter do you want to process? "))
+#    while index < 1 or index > len(parameters):
+#        index = int(input("Try again. Which parameter do you want to process? "))
+#    filename += "_{}".format(parameters[index-1])
+#
+#    rel_arr = rel_data.iloc[:,len(parameters)*(sweep-1) + (index-1)].to_numpy()
+#
+#    clean = pd.DataFrame(data = rel_arr.reshape(plate_rows, plate_cols,order='F'), index=row_names, columns=range(1,plate_cols+1))
+#
+#    clean.to_csv("./output/clean_" + filename + ".csv")
 
 main()
