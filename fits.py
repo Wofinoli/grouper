c5ad56334c079eb380da2bb201492864d0136ac6
Wilmer Finol Inciarte <WilmerFinolInciarte@gmail.com>
2021-05-26 16:57:41 -0500

    Finish function parsing

--- fits.py
+++ fits.py
@@ -1,10 +1,11 @@
+import sys
 import json
 import numpy as np
+import FunctionStringParser
 
 class Fit_Handler():
     def __init__(self):
         self.make_fits()
-        print(self.choose_fit('INA_IV'))
 
     def make_fits(self):
         with open('fits.json') as f:
@@ -24,6 +25,24 @@ class Fit():
         self.name = _name
         self.variables = _variables
         self.control = _control
+        self.func_string = _function
+        self.fsp = FunctionStringParser.FunctionStringParser()
+
+    def eval(self, args):
+        num_var = len(self.variables)
+        num_arg = len(args)
+        if(num_var != num_arg):
+            sys.exit("Not the right amount of arguments. Got {} but expected {}.".format(num_arg,num_var))
+
+        exp_string = "" 
+        for idx in range(0, num_var):
+            if(len(exp_string) == 0):
+                exp_string = self.func_string
+
+            exp_string = exp_string.replace(self.variables[idx], str(args[idx]))
+
+        return self.fsp.eval(exp_string)
+
     
     def __str__(self):
         return "{}\t{}\t{}".format(self.name, self.variables, self.control)
